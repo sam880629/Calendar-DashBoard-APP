5959ff3502ef455e7ded80f9bdb60f8893f6e696
sam880629 <sam880629@gmail.com>
2024-11-04 17:38:54 +0800

    至頂區塊

--- src/components/body/MoonRow.jsx
+++ src/components/body/MoonRow.jsx
@@ -1,26 +1,10 @@
+import { useState, useEffect, useRef, Fragment } from "react";
 import ButtonGroup from "../btn/ButtonGroup";
 import DateItem from "./components/DateItem";
 import moment from "moment";
+
 const buttons = [{ text: "room" }, { text: "floor" }];
 
-const MoonRow = ({ dates }) => {
-  return (
-    <div className="flex w-full">
-      {/* room / floor 按鈕 */}
-      <div className=" py-4 flex gap-4 bg-white items-center border-[#E1E1E1] border-b w-[60px] md:w-[140px] xl:w-[152px]">
-        {/* <ButtonGroup buttons={buttons} /> */}
-      </div>
-    
-      {/* Date items */}
-      <div className="flex-grow flex justify-between">
-        {dates.map((date, index) => {
-          const dateState = dateClassHandler(date.date); 
-          return <DateItem key={index} dateState={dateState} />;
-        })}
-      </div>
-    </div>
-  );
-};
 // 判斷是否為週末
 const isWeekend = (date) => {
   const day = moment(date, "YYYY-M-D").day();
@@ -31,20 +15,67 @@ const isWeekend = (date) => {
 const dateClassHandler = (date) => {
   const mmt = moment(date); // 用 moment 對象解析日期
   const isHoliday = isWeekend(date); // 假日判斷
-  const isToday = mmt.isSame(moment(), 'day'); // 判斷是否是今天
-  const isBeforeToday = mmt.isBefore(moment(), 'day'); // 判斷是否是今天之前
+  const isToday = mmt.isSame(moment(), "day"); // 判斷是否是今天
+  const isBeforeToday = mmt.isBefore(moment(), "day"); // 判斷是否是今天之前
   const weekDay = mmt.format("ddd"); // 星期幾（簡寫）
   const monthName = mmt.format("MMM"); // 月份名稱（簡寫）
   const dateDayName = mmt.format("DD"); // 日期
 
   return {
-      isHoliday,
-      isBeforeToday,
-      isToday,
-      weekDay,
-      monthName,
-      dateDayName
+    isHoliday,
+    isBeforeToday,
+    isToday,
+    weekDay,
+    monthName,
+    dateDayName,
   };
+};
+
+const MoonRow = ({ dates }) => {
+  const moonRowRef = useRef(null); // 參考區塊
+  const [isSticky, setIsSticky] = useState(false); // 控制是否固定
+
+  // 將日期至頂於上方
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsSticky(!entry.isIntersecting); 
+      },
+      { root: null, threshold: 0 } 
+    );
+
+    if (moonRowRef.current) {
+      observer.observe(moonRowRef.current);
+    }
+
+    return () => {
+      if (moonRowRef.current) {
+        observer.unobserve(moonRowRef.current);
+      }
+    };
+  }, []);
+
+  return (
+    <Fragment>
+      <div ref={moonRowRef}></div>
+      <div
+        className={`flex w-full ${isSticky ? "fixed top-0 z-10 bg-white" : ""}`}
+      >
+        {/* room / floor 按鈕 */}
+        <div className=" py-4 flex gap-4 bg-white items-center border-[#E1E1E1] border-b w-[60px] md:w-[140px] xl:w-[152px]">
+          {/* <ButtonGroup buttons={buttons} /> */}
+        </div>
+
+        {/* Date items */}
+        <div className="flex-grow flex justify-between">
+          {dates.map((date, index) => {
+            const dateState = dateClassHandler(date.date);
+            return <DateItem key={index} dateState={dateState} />;
+          })}
+        </div>
+      </div>
+    </Fragment>
+  );
 };
 
 export default MoonRow;
--- src/components/body/TemporaryRow.jsx
+++ src/components/body/TemporaryRow.jsx
@@ -1,45 +1,70 @@
-import { Droppable ,Draggable } from '@hello-pangea/dnd';
-import TemporaryCard from "./components/TemporaryCard"
+import { Droppable, Draggable } from "@hello-pangea/dnd";
+import TemporaryCard from "./components/TemporaryCard";
+import { useState, useEffect, useRef, Fragment } from "react";
 // 暫存區塊
 const TemporaryRow = ({ temporary }) => {
+  const moonRowRef = useRef(null); // 參考區塊
+  const [isSticky, setIsSticky] = useState(false); // 控制是否固定
+
+  // 將日期至頂於上方
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsSticky(!entry.isIntersecting);
+      },
+      { root: null, threshold: 0 }
+    );
+
+    if (moonRowRef.current) {
+      observer.observe(moonRowRef.current);
+    }
+
+    return () => {
+      if (moonRowRef.current) {
+        observer.unobserve(moonRowRef.current);
+      }
+    };
+  }, []);
+
   return (
-    <Droppable droppableId={`tp-drop-0000-00-00-25`} direction="horizontal">
-    {(provided) => (
-      <div
-        className=' h-10  bg-orange-100 flex '
-        {...provided.droppableProps}
-        ref={provided.innerRef}
-       >
-        {temporary.length > 0 ? (
-        temporary.map((booking, index) => (
-          <Draggable
-            key={booking.id}
-            draggableId={`1-booking-${booking.id}`}
-            index={index}
-           
-          >
-            {(provided) => (
-              <div
-                ref={provided.innerRef}
-                {...provided.draggableProps}
-                {...provided.dragHandleProps}
-              >
-                <TemporaryCard 
-                    booking={booking}  
-                    
-                />
-              </div>
+    <Fragment>
+        <div ref={moonRowRef}></div>
+        <Droppable droppableId={`tp-drop-0000-00-00-25`} direction="horizontal">
+        {(provided) => (
+            <div
+            className={`h-10 w-full bg-orange-100 flex ${isSticky ? "fixed top-[72px] z-10" : ""}`}
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+            >
+            {temporary.length > 0 ? (
+                temporary.map((booking, index) => (
+                <Draggable
+                    key={booking.id}
+                    draggableId={`1-booking-${booking.id}`}
+                    index={index}
+                >
+                    {(provided) => (
+                    <div
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                    >
+                        <TemporaryCard booking={booking} />
+                    </div>
+                    )}
+                </Draggable>
+                ))
+            ) : (
+                <div className="flex justify-center items-center font-bold text-sm pl-2">
+                {"temporary Box"}
+                </div>
             )}
-          </Draggable>
-        ))
-      ) : (
-        <div className="flex justify-center items-center font-bold text-sm pl-2" >{"temporary Box"}</div>
-      )}
-        {provided.placeholder}
-        </div>
+            {provided.placeholder}
+            </div>
         )}
-    </Droppable>
-  )
+        </Droppable>
+    </Fragment>
+  );
 };
 
 export default TemporaryRow;
